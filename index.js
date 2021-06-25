ce223d626bb5728794d343dd9ea60f913f50239a
Gabriella-Kate <gabriellakate04@gmail.com>
2021-06-26 00:00:50 +0200

    inital set up &  hero section (responsive)

---
+++ index.js
@@ -0,0 +1,20 @@
+// Header Modal
+let modal = document.getElementById("headerModal");
+let btn = document.getElementById("modalButton");
+let span = document.getElementsByClassName("header__modal__close")[0];
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
